037b47620b0b5da2688ef16904e4bdf1c42d622c
shikariix <sarahsteenhuis@live.nl>
2017-06-21 12:22:54 +0200

    PLANETARY 0.1.9
    Fixed Sticky Tongue with sprite and all.

--- Assets/Scripts/StickyTongue.cs
+++ Assets/Scripts/StickyTongue.cs
@@ -5,34 +5,61 @@ using UnityEngine;
 public class StickyTongue : MonoBehaviour {
 
 	private SpringJoint2D spring;
+	private Animator sprite;
+	public GameObject tongue;
+
+	Vector3 scale;
 	Vector3 targetPos;
 	RaycastHit2D hit;
+
 	public float distance = 10f;
 	public LayerMask mask;
 
-	// Use this for initialization
 	void Start () {
 		spring = GetComponent<SpringJoint2D> ();
+		sprite = GetComponent<Animator> ();
+		sprite.SetBool ("usingTongue", false);
+		tongue.SetActive (false);
+		scale = tongue.transform.localScale;
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () {
 		if (Input.GetButtonDown ("Fire1")) {
+			//change the sprite to an open mouth;
+			sprite.SetBool("usingTongue", true);
+
+			//raycasting
 			targetPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			targetPos.z = 0;
-
 
 			hit = Physics2D.Raycast (transform.position, targetPos-transform.position, distance, mask);
 
 			if (hit.collider != null) {
 				//this should actually connect to a rigidbody;
 				spring.enabled = true;
+				spring.connectedBody = hit.rigidbody;
 				spring.connectedAnchor = targetPos;
+
+				tongue.SetActive(true);
+
 			} 
+
+		}
+		if (Input.GetButton ("Fire1")) {
+			sprite.SetBool ("isIdle", false);
+			sprite.SetBool ("isWalking", false);
+			sprite.SetBool ("isFloating", false);
+
+			tongue.transform.right = targetPos - tongue.transform.position;
+			tongue.transform.position = (transform.position + targetPos) / 2;
+			scale.Set (Vector2.Distance (tongue.transform.position, targetPos)+0.5f, scale.y, scale.z);
+			tongue.transform.localScale = scale;
 		}
 
 		if (Input.GetButtonUp ("Fire1")) {
 			spring.enabled = false;
+			sprite.SetBool ("usingTongue", false);
+			tongue.SetActive(false);
 		}
 	}
 }
